7029b80ee526b55f63f2a097c2a103226ae5b7bb
josephndegwa <kariukindegwa98@gmail.com>
2022-02-12 14:50:48 +0300

    add home page and random quote

---
+++ app/main/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
+from sqlalchemy import desc
+from app.main import bp
+from app import db
+import requests
+from app.models import User, Post, Comment
+from app.main import forms
+
+@bp.route('/')
+def index():
+    # get the quote of the day
+    url = "http://quotes.stormconsultancy.co.uk/random.json"
+    response = requests.get(url).json()
+
+    # get all the posts for displaying
+    posts = Post.query.order_by(desc(Post.timestamp)).all()
+    return render_template('index.html', posts=posts, User=User, quote=response)
