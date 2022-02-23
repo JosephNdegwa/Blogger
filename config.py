ecac4c82f876d13826bcfee40328ba31b3ee0574
josephndegwa <kariukindegwa98@gmail.com>
2022-02-23 22:48:42 +0300

    update script

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can\'t get it'
     #SECRET_KEY = os.urandom(32)
     #config['SECRET_KEY'] = SECRET_KEY
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kariuki_ndegwa:515021@localhost/blogger'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kariuki_ndegwa:515021@localhost/blogger'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User, Post, Comment
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
-app = create_app()
+app = create_app('production')
 
 
 manager = Manager(app)
