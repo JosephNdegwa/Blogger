f5502901b6a4b3e3394fe11fc9a9191dc0b4dfaa
josephndegwa <kariukindegwa98@gmail.com>
2022-02-24 13:51:15 +0300

    solve internal error

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config():
     #SECRET_KEY = os.urandom(32)
     #config['SECRET_KEY'] = SECRET_KEY
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kariuki_ndegwa:515021@localhost/blogger'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
