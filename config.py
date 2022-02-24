1d22637e5a0089d2f735ee3eaf9535b244ad851b
josephndegwa <kariukindegwa98@gmail.com>
2022-02-24 12:38:57 +0300

    update prodconfig

--- config.py
+++ config.py
@@ -2,10 +2,31 @@ from distutils.command.config import config
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-can\'t get it'
     #SECRET_KEY = os.urandom(32)
     #config['SECRET_KEY'] = SECRET_KEY
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kariuki_ndegwa:515021@localhost/blogger'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+    
+    pass
+
+
+
+config_options = {
+'production':ProdConfig
+}
